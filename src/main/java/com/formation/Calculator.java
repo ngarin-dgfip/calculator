3ab74918467b903fc3e3e6760bec033507bcd97d
Raoul <moi@chezmoi.com>
2026-06-12 08:20:34 +0000

    Correction du bug du +1

--- src/main/java/com/formation/Calculator.java
+++ src/main/java/com/formation/Calculator.java
@@ -8,7 +8,7 @@ package com.formation;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b + 1;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
